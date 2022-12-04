dba67032cda544a92078f369e2bde5b986a107c4
Jakub Ambroziak <ambroziak.kuba@wp.pl>
2022-12-04 20:57:24 +0100

    better structure

--- app.js
+++ app.js
@@ -14,16 +14,18 @@ const kayn = Kayn("RGAPI-12ec028c-3dcb-415a-94d6-1906be3fa22f")(/* optional conf
 app.get('/login', (req, res) => {
     
   });
+  app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/" + "/public/home.html");
+  });
 
-//kayn.Summoner.by.name
-
-app.post('/createEmp', function(req, res){  
-    //now req.body will be populated with the object you sent
-    console.log(req.body.name); //prints john
-    });
+  app.post("/request", (req, res) => {
+    res.json([{
+       name_recieved: req.body.name,
+       designation_recieved: req.body.designation
+    }])
+ })
 
 app.listen(8008, () =>
 console.log(
-  'HTTP Server up. Now go to http://localhost:8008/home.html in your browser.'
+  'HTTP Server up. Now go to http://localhost:8008/ in your browser.'
 ));
-
--- public/script.js
+++ public/script.js
@@ -1,6 +1,3 @@
-var xhttp = new XMLHttpRequest();
-this.server.open(this.method, this.url, true);
-
 function searchSummoner() {
 
     let summonerNickname = document.getElementById("summonerNickname").value;
@@ -11,7 +8,14 @@ function searchSummoner() {
 
     var summoner = {"nickname": summonerNickname, "server": server};
     var summonerString = JSON.stringify(summoner);
-    xhttp.send(summonerString);
-
+    
+    $.post("/request",
+    {
+       name: "viSion",
+       designation: "Professional gamer"
+    },
+    function (data, status) {
+       console.log(data);
+    });
 }
 
