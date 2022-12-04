83345c67a7d4087f84d22bda07ea8609a73fb5e1
Jakub Ambroziak <ambroziak.kuba@wp.pl>
2022-12-04 21:59:52 +0100

    better form

--- public/script.js
+++ public/script.js
@@ -1,14 +1,15 @@
-// document.getElementById("submitForm").addEventListener("click", searchSummoner);
+function searchSummoner() {
 
-// function searchSummoner() {
-
-//     let summonerNickname = document.getElementById("summonerNickname").value;
-//     let server= document.getElementById("server").value;
+    let summonerNickname = document.getElementById("summonerNickname").value;
+    let server= document.getElementById("server").value;
     
-//     console.log(summonerNickname)
-//     console.log(server);
+    console.log(summonerNickname)
+    console.log(server);
 
-//     var summoner = {"nickname": summonerNickname, "server": server};
-//     var summonerString = JSON.stringify(summoner);
     
-// }
+}
+
+
+
+//   $( "button" ).on( "click", notify );
+//   console.log("working");
